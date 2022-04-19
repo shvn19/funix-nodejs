07d7aa1d8d94e60342fdca0495e289d9c4df6206
shvn19 <54665750+shvn19@users.noreply.github.com>
2022-04-19 17:32:50 +0700

    first commit

---
+++ first-app.js
@@ -0,0 +1,4 @@
+// import filesystem functionality and call it and store it in simple function
+const fs = require('fs'); //fs stands for file system
+
+fs.writeFileSync('hello.txt', 'Hello from Node.js'); //path to the file including the file name
